d4f20f53f47934819666638c6439c045a0e79069
Cameron Yick <cameron.yick@yale.edu>
2015-08-03 22:23:54 -0400

    Remove vague modules import comment from __init__.py, and add documentation for methods of the MongonEngine to the blog.py route file

--- blask-6/app/__init__.py
+++ blask-6/app/__init__.py
@@ -15,6 +15,9 @@ from app.routes.home import home
 app.register_blueprint(blog)
 app.register_blueprint(home)
 
-# Because our routes are now spread across multiple modules, we need to use the Blueprint name to specify which module the desired route lives in. The string inside a call to url_for is blueprint_name.function_name. We can very easily update home.html to reflect this
+# Because our routes are now spread across multiple modules, we need to use the 
+# #Blueprint name to specify which module the desired route lives in. The string 
+# #inside a call to url_for is blueprint_name.function_name. We can very easily 
+# #update home.html to reflect this
 # OLD: {{ url_for('home_page') }}
 # NEW: {{ url_for('home.home_page') }}
--- blask-6/app/routes/blog.py
+++ blask-6/app/routes/blog.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-#last 3 imports are for the new form
 from app.models.blog import BlogPost, BlogPostForm
 
 # Create the blog Blueprint.  Note that all routes on the blog blueprint have
@@ -9,7 +8,7 @@ blog = Blueprint('blog', __name__, url_prefix='/blog')
 @blog.route('/')  # Accessible at /blog/
 def blog_page():
     """The blog page."""
-    #objects() method comes from the Flask mongoengine
+    # objects() is a Flask-Mongoengine method that returns all of the BlogPost objects.
     posts = BlogPost.objects()
     return render_template('blog.html', posts=posts)
 
@@ -19,13 +18,20 @@ def new():
     form = BlogPostForm(request.form)
 
     if request.method =='POST' and form.validate():
+        # form.save() is a Flask-Mongoengine method that creates an entry in 
+        # mongoDB. The form object is populated by the values entered into the form
+        # on page's view. For secure applications with other databases
+        # , you might add methods here to sanitize the submitted data and reject 
+        # dangerous inputs. See: http://bobby-tables.com/
         form.save()
         return redirect(url_for('blog.blog_page'))
 
     return render_template('new.html', form =form)
 
 @blog.route('/view/<id>')
-# grab postObjectID parameter to have a static URL for people to bookmark.
+# grab post ObjectID parameter to have a static URL for people to bookmark.
+# #If you skip the <id> part of the route, each blog post will appear at 
+# localhost:5000/view/, and only accessible by clicking through the blog page.
 def view(id):
     """View contents of a blog post"""
     # Retrive post comment using mongoEngine
